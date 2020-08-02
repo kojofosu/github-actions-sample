07786743f7ed1c4d1e73d2903025c188353cf445
kojofosu edue <kojofosube@gmail.com>
2020-08-02 21:16:59 +0000

    first push

---
+++ src/main/java/com/example/edue/SpringGithubActionsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.edue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubActionsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubActionsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/edue/SpringGithubActionsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
