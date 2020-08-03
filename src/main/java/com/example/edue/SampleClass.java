ef7f68b4833424990ed816337fc0a2f11e6b4d36
kojofosu edue <kojofosube@gmail.com>
2020-08-03 11:27:19 +0000

    first commit to develop branch

---
+++ src/main/java/com/example/edue/SampleClass.java
@@ -0,0 +1,13 @@
+package com.example.edue;
+
+public class SampleClass {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
